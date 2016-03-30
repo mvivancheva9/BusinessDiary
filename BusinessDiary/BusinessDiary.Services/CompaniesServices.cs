7638d81ddad197e3458751f85c5db087f1530c24
Margarita Ivancheva <mvivancheva@gmail.com>
2016-03-30 19:54:38 +0300

    added company services

---
+++ BusinessDiary/BusinessDiary.Services/CompaniesServices.cs
@@ -0,0 +1,52 @@
+﻿namespace BusinessDiary.Services
+{
+    using System;
+    using System.Linq;
+    using Models;
+    using BusinessDiary.Services.Contracts;
+    using Data.Repositories;
+
+    public class CompaniesServices : ICompaniesServices
+    {
+        private IRepository<Company> companies;
+
+        public CompaniesServices(IRepository<Company> companies)
+        {
+            this.companies = companies;
+        }
+
+        public Company Create(Company company)
+        {
+            this.companies.Add(company);
+            this.companies.SaveChanges();
+            return company;
+        }
+
+        public Company DeleteById(int id)
+        {
+            var companyToDelete = this.companies.GetById(id);
+            this.companies.Delete(companyToDelete);
+            return companyToDelete;
+        }
+
+        public IQueryable<Company> GetAll()
+        {
+            return this.companies.All();
+        }
+
+        public Company GetById(int id)
+        {
+            return this.companies.GetById(id);
+        }
+
+        public Company Update(Company business)
+        {
+            var companyToUpdate = this.companies.GetById(business.Id);
+            companyToUpdate.Name = business.Name;
+
+            this.companies.SaveChanges();
+
+            return companyToUpdate;
+        }
+    }
+}
---
+++ BusinessDiary/BusinessDiary.Services/Contracts/ICompaniesServices.cs
@@ -0,0 +1,18 @@
+﻿namespace BusinessDiary.Services.Contracts
+{
+    using Models;
+    using System.Linq;
+
+    public interface ICompaniesServices
+    {
+        IQueryable<Company> GetAll();
+
+        Company Update(Company business);
+
+        Company DeleteById(int id);
+
+        Company Create(Company company);
+
+        Company GetById(int id);
+    }
+}
